103acf41d06214bf72b378ced6fe6eac749e0122
meet-tola <wasiusikiru7@gmail.com>
2024-08-31 14:21:29 +0100

    post api url fix

--- utils/post.js
+++ utils/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8800";
 
 export const fetchPosts = async (query = {}) => {
   try {
