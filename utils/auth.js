0db5161600af9315f75d202754d9f6ffeaf747cc
meet-tola <wasiusikiru7@gmail.com>
2024-09-01 14:16:23 +0100

    Ensure cookies are sent for cross-site requests

--- utils/auth.js
+++ utils/auth.js
@@ -4,17 +4,20 @@ const API_URL = process.env.NODE_ENV === "production"
   ? process.env.NEXT_PUBLIC_API_URL
   : "http://localhost:8800";
 
-export const checkAuth = async () => {
+export const checkAuth = async (token) => {
   try {
     const response = await axios.get(
       `${API_URL}/api/auth/checkAuth`,
-      { withCredentials: true }
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        withCredentials: true, // Ensure cookies are sent for cross-site requests
+      }
     );
     return response.status === 200;
   } catch (error) {
-    console.log(error);
-    
+    console.error("Authentication check failed:", error);
     return false;
   }
 };
-
